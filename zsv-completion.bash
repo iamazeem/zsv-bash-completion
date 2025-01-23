dd5027dc2db5e0bc1461b364bc7127d240ee1300
Azeem Sajid <azeem.sajid@gmail.com>
2025-01-23 19:21:52 +0500

    Use associative arrays

--- zsv-completion.bash
+++ zsv-completion.bash
@@ -1,44 +1,6 @@
 #!/usr/bin/env bash
 
-MAIN_CMDS=(
-    version
-    help
-    register
-    unregister
-    thirdparty
-    license
-    select
-    echo
-    sql
-    count
-    desc
-    pretty
-    flatten
-    2json
-    2tsv
-    serialize
-    stack
-    compare
-    2db
-    prop
-    rm
-    mv
-    jq
-)
-
-MAIN_CMDS_WITHOUT_MAIN_FLAGS=(
-    version
-    help
-    register
-    unregister
-    thirdparty
-    license
-    prop
-    rm
-    jq
-)
-
-MAIN_FLAGS=(
+GLOBAL_FLAGS="
     -L
     -c
     -r
@@ -65,16 +27,69 @@ MAIN_FLAGS=(
     --keep-blank-headers
     --header-row
     --verbose
+"
+
+declare -A COMMANDS=(
+    [_zsv]="
+        version
+        help
+        register
+        unregister
+        thirdparty
+        license
+        select
+        echo
+        sql
+        count
+        desc
+        pretty
+        flatten
+        2json
+        2tsv
+        serialize
+        stack
+        compare
+        2db
+        prop
+        rm
+        mv
+        jq
+    "
+    [_version]=""
+    [_help]=""
+    [_register]=""
+    [_unregister]=""
+    [_thirdparty]=""
+    [_license]=""
+    [_select]="$GLOBAL_FLAGS"
+    [_echo]="$GLOBAL_FLAGS"
+    [_sql]="$GLOBAL_FLAGS"
+    [_count]="$GLOBAL_FLAGS"
+    [_desc]="$GLOBAL_FLAGS"
+    [_pretty]="$GLOBAL_FLAGS"
+    [_flatten]="$GLOBAL_FLAGS"
+    [_2json]="$GLOBAL_FLAGS"
+    [_2tsv]="$GLOBAL_FLAGS"
+    [_serialize]="$GLOBAL_FLAGS"
+    [_stack]="$GLOBAL_FLAGS"
+    [_compare]="$GLOBAL_FLAGS"
+    [_2db]="$GLOBAL_FLAGS"
+    [_prop]=""
+    [_rm]=""
+    [_mv]=""
+    [_jq]=""
 )
 
-__zsv_completions() {
+_zsv() {
+    COMPREPLY=()
     if ((COMP_CWORD == 1)); then
-        COMPREPLY=($(compgen -W "${MAIN_CMDS[*]}" -- "${COMP_WORDS[1]}"))
-    elif ((COMP_CWORD >= 2)) && [[ ${COMP_WORDS[$COMP_CWORD]} == "-"* ]]; then
-        if ! echo "${MAIN_CMDS_WITHOUT_MAIN_FLAGS[*]}" | grep -o -- "${COMP_WORDS[1]}" >/dev/null; then
-            COMPREPLY=($(compgen -W "${MAIN_FLAGS[*]}" -- "${COMP_WORDS[$COMP_CWORD]}"))
+        COMPREPLY=($(compgen -W "${COMMANDS[_zsv]}" -- "${COMP_WORDS[1]}"))
+    elif ((COMP_CWORD >= 2)) && [[ ${COMP_WORDS[$COMP_CWORD]} == -* ]]; then
+        local SUBCOMMAND="${COMP_WORDS[1]}"
+        if [[ -n ${COMMANDS[_$SUBCOMMAND]} ]]; then
+            COMPREPLY=($(compgen -W "${COMMANDS[_$SUBCOMMAND]}" -- "${COMP_WORDS[$COMP_CWORD]}"))
         fi
     fi
 }
 
-complete -o bashdefault -o default -F __zsv_completions zsv
+complete -o bashdefault -o default -F _zsv zsv
